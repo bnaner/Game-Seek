f5948d81c685a3684dc0ad569a9cd0e74fde434d
Layth <148519027+bnaner@users.noreply.github.com>
2024-12-02 18:45:08 -0500

    Added Basic Files

---
+++ Node.cpp
@@ -0,0 +1,5 @@
+//
+// Created by layth on 12/2/2024.
+//
+
+#include "Node.h"
---
+++ Node.h
@@ -0,0 +1,12 @@
+//
+// Created by layth on 12/2/2024.
+//
+
+#ifndef GAME_SEEK_NODE_H
+#define GAME_SEEK_NODE_H
+
+#endif //GAME_SEEK_NODE_H
+
+class Node {
+
+};
